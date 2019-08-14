590aea2fc2feb4caea09c8214fb9e2c93df90312
Benjamin <newuser@News-MBP-2.attlocal.net>
2019-08-13 17:17:21 -0700

    added custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,25 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ test the email for a new user is normalized """
+        email = 'test@GMAIL.COM'
+        user = get_user_model().objects.create_user(email, 'testpassword123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ test that creating user with no email raises an error """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'testpassword123')
+
+    def test_create_new_superuser(self):
+        """ test creating a new superuser """
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'testpassword123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
